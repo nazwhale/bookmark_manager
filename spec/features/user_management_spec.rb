f7d023fbdbe73c5f12dd6b3cd243033cf8a528eb
Naz <nazmalik08@gmail.com>
2017-04-07 12:44:52 +0100

    add verification for email format

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -5,6 +5,24 @@ feature 'user sign up' do
     expect(page).to have_content 'Password and confirmation password do not match'
   end
 
+  scenario 'user cant sign up without entering an email' do
+  visit '/sign_in'
+    fill_in(:email, with: '')
+    fill_in(:password, with: 'cats')
+    fill_in(:password_confirmation, with: 'cats')
+    click_button 'Submit'
+    expect{ sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario 'user cant sign up with invalid email format' do
+  visit '/sign_in'
+    fill_in(:email, with: 'invalid@email')
+    fill_in(:password, with: 'cats')
+    fill_in(:password_confirmation, with: 'cats')
+    click_button 'Submit'
+    expect{ sign_up(email: "invalid@email") }.not_to change(User, :count)
+  end
+
 def sign_up(email: 'nazwhale@gmail.com', password: 'vimmaster', password_confirmation: 'vimmaster')
   visit '/sign_in'
   fill_in(:email, with: email)
